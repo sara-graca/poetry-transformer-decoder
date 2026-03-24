3d2fe3515319615f59765ed06ede648c63ac20a9
sara-graca <saravsgraca@gmail.com>
2026-03-24 16:38:59 +0100

    Preprocessing cautionary tales

---
+++ preprocess.py
@@ -0,0 +1,20 @@
+with open('cautionary_tales.txt', 'r', encoding='utf-8') as f:
+    corpus = f.read()
+
+books = corpus.split("*** END OF")
+books = [b.split("*** START OF")[-1] for b in books]
+
+cleaned_books = []
+for book in books:
+    lines = book.split("\n")
+    cleaned = []
+    for line in lines:
+        if "[illustration" in line.lower():
+            continue
+        cleaned.append(line)
+    cleaned_books.append("<book>\n" + "\n".join(cleaned) + "\n</book>")
+
+result = "\n\n".join(cleaned_books)
+
+with open('corpus.txt', 'w', encoding='utf-8') as f:
+    f.write(result)
